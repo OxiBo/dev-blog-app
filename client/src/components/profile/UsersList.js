bb30a847fa026f284bfef8927fc2703a33269ca3
OxiBo <xenos2000@ukr.net>
2020-05-25 23:59:57 -0400

    Add spinner to userList

--- client/src/components/profile/UsersList.js
+++ client/src/components/profile/UsersList.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchUsers } from "../../actions";
+import { fetchUsers, setSpinner } from "../../actions";
+import Spinner from "../Spinner";
 import UserCompact from "./UserCompact";
 
 class UsersList extends Component {
-  componentDidMount() {
-    this.props.fetchUsers();
+  async componentDidMount() {
+    await this.props.setSpinner();
+    await this.props.fetchUsers();
+    this.props.setSpinner(false);
   }
 
   render() {
@@ -16,22 +19,30 @@ class UsersList extends Component {
             <h1 className="card-title text-center m-4">Our Bloggers</h1>
           </div>
 
-          {this.props.usersList.length > 0 &&
+          {this.props.isLoading ? (
+            <Spinner />
+          ) : (
+            this.props.usersList.length > 0 &&
             this.props.usersList.map(({ bio, _id, createdAt }, index) => (
-              <UserCompact key={bio.email + index} user={{ ...bio, _id, createdAt }} />
-            ))}
+              <UserCompact
+                key={bio.email + index}
+                user={{ ...bio, _id, createdAt }}
+              />
+            ))
+          )}
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ auth }) => {
+const mapStateToProps = ({ auth, spinner }) => {
   return {
-    usersList: auth.users
+    isLoading: spinner.isLoading,
+    usersList: auth.users,
   };
 };
 
-export default connect(mapStateToProps, { fetchUsers })(UsersList);
+export default connect(mapStateToProps, { fetchUsers, setSpinner })(UsersList);
 
 //  return <UserCompact key={user.email} user={user} />;
